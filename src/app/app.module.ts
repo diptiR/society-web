45344061ee66d75c39a2a0635d1df0c8d9a5806e
Dipti Redkar <redkar_d@India.XoriantCorp.com>
2018-05-27 17:44:22 +0530

    Added room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {AppRoutingModule} from './app.routing.module';
 import { AppComponent } from './app.component';
 import { OwnerComponent } from './owner/owner.component';
+import { AddRoomsComponent } from './add-rooms/add-rooms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OwnerComponent
+    OwnerComponent,
+    AddRoomsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {OwnerComponent} from './owner/owner.component';
+import {AddRoomsComponent} from './add-rooms/add-rooms.component';
 const routes: Routes = [
 { path: 'owner', component: OwnerComponent },
+{ path: 'addRooms', component: AddRoomsComponent },
 {path : '', component : OwnerComponent}
 ];
 @NgModule({
